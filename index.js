e3737a366b1eb545b4a8b33d14c8d62962eaeec7
Brenn Kucey <bkucey@users.noreply.github.com>
2018-11-20 18:52:07 -0800

    Take out javascript

--- index.js
+++
@@ -1,9 +0,0 @@
-var outputTime = () => {
-        var d = new Date();
-        const est_options = { timeZone: "America/New_York" }
-        document.getElementById("date").innerHTML = d.toLocaleString({}, est_options);
-};
-
-document.addEventListener("DOMContentLoaded", function (event) {
-        const myInterval = setInterval(outputTime, 1000);
-});
